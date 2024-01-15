1ce74c02f0a00d0ebeb4e7969c300e0a4c4b606a
zaiets.andrii <zaiets.andrii@globaldev.tech>
2024-01-15 17:59:43 +0200

    Initial commit

---
+++ toDoList.js
@@ -0,0 +1,56 @@
+const addInput = document.querySelector('#todoInput'),
+    submit = document.querySelector('#submit'),
+    toDoList = document.querySelector('#todoList'),
+    toDoListItems = document.querySelectorAll('.todo-item'),
+    form = document.querySelector('#addTodoForm');
+
+
+//add list item
+function addListItem(listItems, dataBase) {
+    listItems.innerHTML = "";
+    dataBase.forEach((taskItem, i) => {
+        listItems.innerHTML += `
+           <li class="todo-item">${i + 1} ${taskItem}
+           <button class="delete-btn">Delete</button>
+           </li>
+           `;
+    });
+
+    document.querySelectorAll('.delete-btn').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            storage.splice(i, 1);
+
+            addListItem(listItems, dataBase);
+        })
+    })
+}
+
+const taskDB = {
+    tasks: []
+}
+const storage = taskDB.tasks;
+
+//add value listener
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let task = addInput.value;
+    if (task && task.length < 21) {
+        storage.push(task);
+        console.log(task.length);
+    }
+    else {
+        alert("Not correct value");
+    }
+
+    addListItem(toDoList, storage);
+    e.target.reset();
+
+    console.log(storage);
+});
+
+
+
+
+
